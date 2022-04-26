3c15e636319c6b8d20c08020479cb05eb7b382ca
Ruben Guzman <rubenguzman48@gmail.com>
2022-04-26 00:36:06 -0300

    commit master 00.36

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -1,9 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #define N 20
-int i;
-double vt[N];
-…
-for(i = 0;i<N; i++)
-{
-vt[i]=1+rand()%100;
-printf(“%d ”, vt[i]);
-}
+
+int main()
+{    
+    int i;
+    int vt[N];
+    
+
+    for(i = 0;i<N; i++)
+    {
+        vt[i]=1+rand()%100;
+        printf("\n %d ", vt[i]);
+    }
+
+return 0;
+}    
